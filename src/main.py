2840a6a1aab9e1cc0a884d501741b66dfe4f300a
0046tkhr <oosrtkhr.2004@gmail.com>
2023-02-15 16:15:01 +0900

    不要なログ再削除

--- src/main.py
+++ src/main.py
@@ -112,7 +112,6 @@ def login():
 
 @app.route("/api/schoolappParent/createParent", methods=["POST"])
 def createParent():
-    print('createParent')
     # payloadの取得
     event = request.get_json()
     parent_name = event['parentName']
@@ -144,7 +143,6 @@ def createParent():
 
 @app.route("/api/schoolappParent/searchParentByUserId", methods=["POST"])
 def searchParentByUserId():
-    print('searchParentByUserId')
     # userIdの取得
     event = request.get_json()
     userId = event['userId']
@@ -170,7 +168,6 @@ def searchParentByUserId():
 
 @app.route("/api/schoolappParent/searchStudentsByParentId", methods=["POST"])
 def searchStudentByParentId():
-    print('searchStudentByParentId')
     # userIdの取得
     event = request.get_json()
     parent_id = event['parentId']
@@ -202,8 +199,6 @@ def searchStudentByParentId():
 
 @app.route("/api/schoolappParent/linkRelation", methods=["POST"])
 def linkRelation():
-    print('linkRelation')
-    
     # eventから各値を取得
     event = request.get_json()
     parent_id = event['parentId']
@@ -231,13 +226,11 @@ def linkRelation():
 
 @app.route("/api/schoolappParent/searchDelivery", methods=["POST"])
 def search_delivery():
-    print('search_delivery')
     dynamodb = boto3.resource('dynamodb')
 
     # リクエストから値を取得
     event = request.get_json()
     parent_id = event['parent_id']
-    print
     student_id = event['student_id']
 
     # 生徒に紐づく配信idのリストを取得
